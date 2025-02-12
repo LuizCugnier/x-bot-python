f1e78cd2e1d75abed25e17a6462f982a3a9448c3
LuizCugnier <luizcugnier.neto@gmail.com>
2025-02-12 06:45:30 -0300

    update to add account name and show accounts registered in db

--- website/models.py
+++ website/models.py
@@ -1,5 +1,10 @@
 from . import db
 from flask_login import UserMixin
+from cryptography.fernet import Fernet
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY") or Fernet.generate_key()
+cipher = Fernet(SECRET_KEY)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,9 +14,18 @@ class User(db.Model, UserMixin):
 
 class TwitterAccounts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    acces_token = db.Column(db.String(250))
-    acces_token_secret = db.Column(db.String(250))
-    api_token = db.Column(db.String(250))
-    api_token_secret = db.Column(db.String(250))
-    beare_token = db.Column(db.String(250))
+    account_name = db.Column(db.String(150))
+    access_token = db.Column(db.String(250))
+    access_token_secret = db.Column(db.String(250))
+    api_key = db.Column(db.String(250))
+    api_key_secret = db.Column(db.String(250))
+    bearer_token = db.Column(db.String(250))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def encrypt_data(self, data):
+        """Criptografa um valor antes de armazená-lo"""
+        return cipher.encrypt(data.encode()).decode()
+
+    def decrypt_data(self, data):
+        """Descriptografa um valor para uso"""
+        return cipher.decrypt(data.encode()).decode()
--- website/views.py
+++ website/views.py
@@ -1,9 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import db, TwitterAccounts, User
+from cryptography.fernet import Fernet
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 @login_required
 def home():
-    return render_template("index.html", user=current_user)
+    accounts = TwitterAccounts.query.all() 
+    return render_template("index.html", user=current_user, accounts=accounts)
+
+@views.route('/register-x-account', methods=['GET', 'POST'])
+@login_required
+def register_x_account():
+    if request.method == 'POST':
+        data = request.form
+
+        new_account = TwitterAccounts(
+            account_name=data['account_name'],
+            access_token=TwitterAccounts.encrypt_data(TwitterAccounts, data['access_token']),
+            access_token_secret=TwitterAccounts.encrypt_data(TwitterAccounts, data['access_token_secret']),
+            api_key=TwitterAccounts.encrypt_data(TwitterAccounts, data['api_key']),
+            api_key_secret=TwitterAccounts.encrypt_data(TwitterAccounts, data['api_key_secret']),
+            bearer_token=TwitterAccounts.encrypt_data(TwitterAccounts, data['bearer_token']),
+            user_id=current_user.id
+        )
+
+        db.session.add(new_account)
+        db.session.commit()
+        flash("X Account registered successfully!", "success")
+        return redirect(url_for('views.home'))
+
+    return render_template("register-x-account.html", user=current_user)
+
+@views.route('/registered-x-accounts')
+@login_required
+def registered_x_accounts():
+    accounts = TwitterAccounts.query.join(User).all()
+    
+    return render_template("registered-x-accounts.html", user=current_user, accounts=accounts)
